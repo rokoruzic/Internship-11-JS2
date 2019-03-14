25d3ae7ec9a9c887ecf04838e445d438ea303b5c
rokoruzic <roko.ruzic4@gmail.com>
2019-03-14 22:11:21 +0100

    3.task done

--- script.js
+++ script.js
@@ -53,38 +53,37 @@ createLiElements(sleeveLenghtUlElement, sleeevLenghtArray);
 function createOfferElements(array) {
   var mainOffersElement = document.getElementById("offers");
   array.forEach(function(offer) {
-
     let div = document.createElement("div");
-    div.classList.add("offers_item");
+    div.classList.add("offers__item");
 
-    let itemImg=document.createElement("img");
+    let itemImg = document.createElement("img");
+    itemImg.src = "./assets/images/offer.jpg";
+    itemImg.alt = "Offer";
     itemImg.classList.add("item__img");
-    itemImg.src="assets/images/offer.jpg";
-    itemImg.alt="Offer";
 
     let heartShapeImg = document.createElement("img");
-    heartShapeImg.classList.add("item_heart-shape");
-    heartShapeImg.src = "assets/images/offer.jpg";
+    heartShapeImg.src = "./assets/images/heart-shape.png";
     heartShapeImg.alt = "Hearth Shape";
+    heartShapeImg.classList.add("item__heart-shape");
 
     let heartFullImg = document.createElement("img");
-    heartFullImg.classList.add("item_heart-full");
-    heartFullImg.src = "./assets/images/heart-shape.png";
+    heartFullImg.src = "./assets/images/heart-full.png";
     heartFullImg.alt = "Hearth Full";
+    heartFullImg.classList.add("item__heart-full");
 
     let descriptionP = document.createElement("p");
-    descriptionP.classList.add("item_img-description");
     descriptionP.innerHTML = "TALL";
+    descriptionP.classList.add("item__img-description");
 
     let paragraphP = document.createElement("p");
-    paragraphP.classList.add("item__paragraph");
     paragraphP.innerHTML = `<span class=item__paragraph__brand> ${
       offer.brand
     } </span>  ${offer.description}`;
+    paragraphP.classList.add("item__paragraph");
 
     let priceSpan = document.createElement("span");
-    priceSpan.classList.add("item__price");
     priceSpan.innerHTML = `${offer.price}`;
+    priceSpan.classList.add("item__price");
 
     div.appendChild(itemImg);
     div.appendChild(heartShapeImg);
@@ -93,7 +92,6 @@ function createOfferElements(array) {
     div.appendChild(paragraphP);
     div.appendChild(priceSpan);
     mainOffersElement.appendChild(div);
-
   });
 }
 
@@ -136,3 +134,94 @@ var offersArray = [
   }
 ];
 createOfferElements(offersArray);
+//3.zadatak.
+var dropDownTopAllDivElementsCount = document.getElementsByClassName("dropdown__top__all").length;
+for (let i = 0; i < dropDownTopAllDivElementsCount; i++) {
+  let dropDownTopAllDivElements = document.getElementsByClassName("dropdown__top__all")[i];
+
+  dropDownTopAllDivElements.addEventListener("mouseenter", function () {
+    let allImgElements = document.getElementsByClassName("all__img")[i];
+    let allTextElements = document.getElementsByClassName("all__text")[i];
+    dropDownTopAllDivElements.classList.add("dropdown__top__all__hover");
+    allImgElements.classList.add("dropdown__top__all__hover__all__img");
+    allTextElements.classList.add("dropdown__top__all__hover__all__text");
+  });
+
+  dropDownTopAllDivElements.addEventListener("mouseleave", function () {
+    let allImgElements = document.getElementsByClassName("all__img")[i];
+    let allTextElements = document.getElementsByClassName("all__text")[i];
+    dropDownTopAllDivElements.classList.remove("dropdown__top__all__hover");
+    allImgElements.classList.remove("dropdown__top__all__hover__all__img");
+    allTextElements.classList.remove("dropdown__top__all__hover__all__text");
+  });
+}
+
+var containerSearchFilterElementsCount = document.getElementsByClassName("container__search-filter").length;
+for (let i = 0; i < containerSearchFilterElementsCount; i++) {
+
+  let containerSearchFilterElements = document.getElementsByClassName("container__search-filter")[i];
+
+  containerSearchFilterElements.addEventListener("mouseenter", function () {
+    let searchFilterImgElements = document.getElementsByClassName("search-filter__img")[i];
+    let searchFilterTextElements = document.getElementsByClassName("search-filter__text")[i];
+    let searchFilterDropdownElements = document.getElementsByClassName("search-filter__dropdown")[i];
+    searchFilterImgElements.classList.add("container__search-filter__hover__search-filter__img");
+    searchFilterTextElements.classList.add("container__search-filter__hover__search-filter__text");
+    containerSearchFilterElements.classList.add("container__search-filter__hover");
+    searchFilterDropdownElements.classList.add("container__search-filter__hover__search-filter__dropdown");
+  });
+
+  containerSearchFilterElements.addEventListener("mouseleave", function () {
+    let searchFilterImgElements = document.getElementsByClassName("search-filter__img")[i];
+    let searchFilterTextElements = document.getElementsByClassName("search-filter__text")[i];
+    let searchFilterDropdownElements = document.getElementsByClassName("search-filter__dropdown")[i];
+    searchFilterImgElements.classList.remove("container__search-filter__hover__search-filter__img");
+    searchFilterTextElements.classList.remove("container__search-filter__hover__search-filter__text");
+    containerSearchFilterElements.classList.remove("container__search-filter__hover");
+    searchFilterDropdownElements.classList.remove("container__search-filter__hover__search-filter__dropdown");
+  });
+}
+
+var dropdownListItemElementsCount = document.getElementsByClassName("dropdown__list__item").length;
+for(let i = 0;i<dropDownTopAllDivElementsCount;i++)
+{
+
+let dropdownListItemElements = document.getElementsByClassName("dropdown__list__item")[i];
+dropdownListItemElements.addEventListener("mouseenter",function(){
+let listItemCountElements=document.getElementsByClassName("list__item__count")[i];
+  dropdownListItemElements.classList.add("dropdown__list__item__hover");
+  listItemCountElements.classList.add("dropdown__list__item__hover__list__item__count");
+
+});
+
+dropdownListItemElements.addEventListener("mouseleave",function(){
+  let listItemCountElements=document.getElementsByClassName("list__item__count")[i];
+    dropdownListItemElements.classList.remove("dropdown__list__item__hover");
+    listItemCountElements.classList.remove("dropdown__list__item__hover__list__item__count");
+  
+  });
+}
+var offersItemElementsCount = document.getElementsByClassName("offers__item").length;
+for (let i = 0; i < offersItemElementsCount; i++) {
+  let offersItemElements = document.getElementsByClassName("offers__item")[i];
+  offersItemElements.addEventListener("mouseenter", function () {
+    let itemHeartFullElement = document.getElementsByClassName("item__heart-full")[i];
+    let itemImgDescriptionElement = document.getElementsByClassName("item__img-description")[i];
+    itemHeartFullElement.classList.add("offers__item__hover__item__heart-full");
+    itemImgDescriptionElement.classList.add("offers__item__hover__item__img-description");
+
+
+  });
+  offersItemElements.addEventListener("mouseleave", function () {
+    let itemHeartFullElement = document.getElementsByClassName("item__heart-full")[i];
+    let itemImgDescriptionElement = document.getElementsByClassName("item__img-description")[i];
+    itemHeartFullElement.classList.remove("offers__item__hover__item__heart-full");
+    itemImgDescriptionElement.classList.remove("offers__item__hover__item__img-description");
+
+
+
+  });
+
+
+}
+
